57ae9a7b25a907fbc4573b42e1df20b5a5bea8b8
unisexx <unisexx@hotmail.com>
2013-02-22 13:37:58 +0700

    add control

---
+++ application/modules/child/views/unsuitable_index.php
@@ -0,0 +1,134 @@
+<h2>ข้อมูลกลุ่มเป้าหมาย - เด็กและเยาวชน</h2>
+<h4>เด็กและเยาวชนที่มีพฤติกรรมไม่เหมาะสมและพบเห็นได้ในที่สาธารณะ <span class="gray">แบบ อปท.1 (2)</span> </h4>
+<div id="search">
+  <div id="searchBox">หมายเลข/หัวหน้าสำนักปลัด
+    <input type="text" name="textfield" id="textfield" style="width:240px;" />
+    <select name="select6" id="select6">
+      <option>-- ทุกปี --</option>
+      <option>2556</option>
+      <option>2555</option>
+      <option>2554</option>
+    </select>
+    <select name="select" id="select">
+      <option>-- ทุกจังหวัด --</option>
+    </select>
+    <select name="select2" id="select2">
+      <option>-- ทุกอำเภอ --</option>
+    </select>
+    <select name="select3" id="select3">
+      <option>-- ทุก อปท. --</option>
+    </select>
+    <select name="select4" id="select4">
+      <option>-- ทุกขนาด --</option>
+    </select>
+<input type="submit" name="button9" id="button9" title="ค้นหา" value=" " class="btn_search" /></div>
+</div>
+
+<div id="btnBox"><input type="button" title="นำเข้าข้อมูล"  value=" " onclick="document.location='people.php?act=import'" class="btn_import"/><input type="button" title="เพิ่มรายการ"  value=" " onclick="document.location='child/unsuitable_form'" class="btn_add"/></div>
+
+<div class="pagebarUTH">&nbsp;<span class="this-page">1</span>
+<a href="javascript:;" title="Seite 2">2</a>
+<a href="javascript:;" title="Seite 3">3</a>
+<a href="javascript:;" title="Seite 4">4</a>
+
+<span class="break">...</span><a href="javascript:;" title="Seite 19">19</a>
+<a href="javascript:;" title="Seite 2">Next</a>&nbsp;&nbsp;188 record
+</div>	
+<table class="tblist">
+<tr>
+  <th>ลำดับ</th>
+  <th>ปี</th>
+  <th><span class="vtip" title="ดื่มเครื่องดื่มที่มีแอลกอฮอล์  สูบบุหรี่ และติดสารเสพติดร้ายแรง 
+เช่น ยาบ้า ยาอี สารระเหย กัญชา เป็นต้น">ดื่มเครื่องดื่ม..</span></th>
+  <th><span class="vtip" title="มั่วสุมและทำความรำคาญให้กับชาวบ้าน">มั่วสุมและ..</span></th>
+  <th><span class="vtip" title="ติดเกมส์ และเล่นการพนันต่าง ๆ">ติดเกมส์ และ...</span></th>
+  <th><span class="vtip" title="มีพฤติกรรมทางเพศ">มีพฤติกรรม..</span></th>
+  <th>อื่นๆ ระบุ</th>
+  <th>หมายเลข</th>
+  <th>อปท.</th>
+  <th>อำเภอ / จังหวัด</th>
+  <th>ขนาด</th>
+  <th>หัวหน้าสำนักปลัด</th>
+  <th>จัดการ</th>
+</tr>
+<tr>
+  <td>1</td>
+  <td>2555</td>
+  <td>3</td>
+  <td>2</td>
+  <td>10</td>
+  <td>1</td>
+  <td>&nbsp;</td>
+  <td>4787</td>
+  <td>บางกระสอบ</td>
+  <td>พระประแดง / สมุทรปราการ</td>
+  <td>เล็ก</td>
+  <td>นายวิรัตน์ ศิโรดม</td>
+  <td><input type="submit" name="button9" id="button9" title="แก้ไขรายการนี้" value=" " class="btn_edit vtip"  onclick="window.location='people.php?act=form'" />
+    <input type="submit" name="button4" id="button4" title="ลบรายการนี้" value=" " class="btn_delete vtip" /></td>
+</tr>
+<tr class="odd">
+  <td>2</td>
+  <td>2555</td>
+  <td>1</td>
+  <td>2</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>564</td>
+  <td>เสม็ดใต้</td>
+  <td>บางคล้า / ฉะเชิงเทรา</td>
+  <td>กลาง</td>
+  <td>นางสาวณัชชารีย์ นรภัทร์จารีย์</td>
+  <td><input type="submit" name="button" id="button" title="แก้ไขรายการนี้" value=" " class="btn_edit vtip"  onclick="window.location='people.php?act=form'" />
+    <input type="submit" name="button" id="button5" title="ลบรายการนี้" value=" " class="btn_delete vtip" /></td>
+  </tr>
+<tr>
+  <td>3</td>
+  <td>2555</td>
+  <td>0</td>
+  <td>4</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>7180</td>
+  <td>เทศบาลตำบลศาลา</td>
+  <td>เกาะคา / ลำปาง</td>
+  <td>ใหญ่</td>
+  <td>นายมณเทียร เสริมไทยสงค์</td>
+  <td><input type="submit" name="button2" id="button2" title="แก้ไขรายการนี้" value=" " class="btn_edit vtip"  onclick="window.location='people.php?act=form'" />
+    <input type="submit" name="button2" id="button6" title="ลบรายการนี้" value=" " class="btn_delete vtip" /></td>
+  </tr>
+<tr class="odd">
+  <td>4</td>
+  <td>2555</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td><input type="submit" name="button3" id="button3" title="แก้ไขรายการนี้" value=" " class="btn_edit vtip"  onclick="window.location='people.php?act=form'" />
+    <input type="submit" name="button3" id="button7" title="ลบรายการนี้" value=" " class="btn_delete vtip" /></td>
+  </tr>
+<tr>
+  <td>5</td>
+  <td>2555</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+  <td><input type="submit" name="button5" id="button8" title="แก้ไขรายการนี้" value=" " class="btn_edit vtip"  onclick="window.location='people.php?act=form'" />
+    <input type="submit" name="button5" id="button10" title="ลบรายการนี้" value=" " class="btn_delete vtip" /></td>
+  </tr>
+</table>

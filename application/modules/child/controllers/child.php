531c0933255b68a3b1fdcafb2d747c58c2036ac8
unknown <Yotsakon@.(none)>
2013-04-19 12:02:00 +0700

    update child/unsuitable update location

--- application/modules/child/controllers/child.php
+++ application/modules/child/controllers/child.php
@@ -61,27 +61,22 @@ Class Child extends Public_Controller{
 		$this->template->build('birth_form');
 	}
 	
-	function unsuitable(){
-	    /*
-	    $sql = 'SELECT 
-          FORM_ALL."ID",
-          FORM_ALL."YEAR",
-          (FORM_ALL.T4161_M + FORM_ALL.T4161_F) AS TOTAL_1,
-          (FORM_ALL.T4162_M + FORM_ALL.T4162_F) AS TOTAL_2,
-          (FORM_ALL.T4163_M + FORM_ALL.T4163_F) AS TOTAL_3,
-          (FORM_ALL.T4164_M + FORM_ALL.T4164_F) AS TOTAL_4,
-          (FORM_ALL.T4165_M + FORM_ALL.T4165_F) AS TOTAL_5,
-          FORM_ALL.NUMBER_ID,
-          FORM_ALL.OPT_NAME,
-          FORM_ALL.AMPHOR,
-          FORM_ALL.PROVINCE,
-          FORM_ALL."SIZE",
-          FORM_ALL.C_TITLE,
-          FORM_ALL.C_NAME
-        FROM FORM_ALL
-        WHERE (FORM_ALL.T4161_M + FORM_ALL.T4161_F + FORM_ALL.T4162_M + FORM_ALL.T4162_F + FORM_ALL.T4163_M + FORM_ALL.T4163_F + FORM_ALL.T4164_M + FORM_ALL.T4164_F + FORM_ALL.T4165_M + FORM_ALL.T4165_F) > 0
-        ORDER BY FORM_ALL."YEAR" DESC, FORM_ALL.NUMBER_ID DESC';
-        */
+	function unsuitable()
+	{
+	    $where = '';
+        if(!empty($_GET))
+        {
+            if(!empty($_GET['keyword']))
+            {
+                $where .= " AND ( FORM_ALL.NUMBER_ID LIKE '%".$_GET['keyword']."%'";
+                $where .= " OR FORM_ALL.C_NAME LIKE '%".$_GET['keyword']."%'";
+                $where .= " OR FORM_ALL.OPT_NAME LIKE '%".$_GET['keyword']."%' )";
+            }
+            
+            if(!empty($_GET['year'])) $where .= ' AND FORM_ALL."YEAR" = '.$_GET['year'];
+            if(!empty($_GET['province_id'])) $where .= ' AND FORM_ALL.PROVINCE_ID = '.$_GET['province_id'];
+            if(!empty($_GET['amphur_id'])) $where .= ' AND FORM_ALL.AMPHUR_ID = '.$_GET['amphur_id'];
+        }
         $sql = 'SELECT 
           FORM_ALL."ID",
           FORM_ALL."YEAR",
@@ -92,22 +87,43 @@ Class Child extends Public_Controller{
           (FORM_ALL.T4165_M + FORM_ALL.T4165_F) AS TOTAL_5,
           FORM_ALL.NUMBER_ID,
           FORM_ALL.OPT_NAME,
-          FORM_ALL.AMPHOR,
-          FORM_ALL.PROVINCE,
+          AMPHUR.AMPHUR_NAME,
+          PROVINCES.PROVINCE,
           FORM_ALL."SIZE",
           FORM_ALL.C_TITLE,
           FORM_ALL.C_NAME
         FROM FORM_ALL 
+        LEFT JOIN PROVINCES ON PROVINCES.ID = FORM_ALL.PROVINCE_ID
+        LEFT JOIN AMPHUR ON AMPHUR.ID = FORM_ALL.AMPHUR_ID
+        WHERE 1=1 '.$where.' 
         ORDER BY FORM_ALL."YEAR" DESC, FORM_ALL.NUMBER_ID DESC';
+        // WHERE (FORM_ALL.T4161_M + FORM_ALL.T4161_F + FORM_ALL.T4162_M + FORM_ALL.T4162_F + FORM_ALL.T4163_M + FORM_ALL.T4163_F + FORM_ALL.T4164_M + FORM_ALL.T4164_F + FORM_ALL.T4165_M + FORM_ALL.T4165_F) > 0
 		$data['result'] = $this->opt->get($sql);
         $data['pagination'] = $this->opt->pagination;
+        $this->template->append_metadata('<script type="text/javascript" src="media/js/jquery.chainedSelect.min.js"></script>');
 		$this->template->build('unsuitable_index', $data);
 	}
 	
 	function unsuitable_form($id = null){
+	    if($_POST)
+        {
+            $this->opt->save($_POST);
+            set_notify('success', lang('save_data_complete'));
+            redirect('child/unsuitable');
+        }
 	    $data['rs'] = $this->opt->get_row($id);
         $this->template->append_metadata('<script type="text/javascript" src="media/js/jquery.chainedSelect.min.js"></script>');
 		$this->template->build('unsuitable_form', $data);
 	}
+
+    function unsuitable_delete($id)
+    {
+        if(!empty($id))
+        {
+            $this->opt->delete($id);
+            set_notify('success', lang('delete_data_complete'));
+        }
+        redirect('child/unsuitable');
+    }
 }
 ?>
--- application/modules/location/controllers/location.php
+++ application/modules/location/controllers/location.php
@@ -8,9 +8,10 @@ class Location extends Controller
 	
 	function ajax_amphur($type = NULL)
 	{
-		$text = ($type == 'report') ? '- ทั้งหมด -' : '- เลือกอำเภอ -';
+		$text = ($type == 'report') ? '-- ทุกอำเภอ --' : '- เลือกอำเภอ -';
 		$result = $this->db->GetArray('select id,amphur_name as text from amphur where province_id = ? order by amphur_name',$_GET['q']);
         dbConvert($result);
+        if($type == 'report' and !empty($_GET['q'])) array_unshift($result, array('id' => '', 'text' => $text));
 		echo $result ? json_encode($result) : '[{"id":"","text":"'.$text.'"}]';
 	}
 	
@@ -20,18 +21,4 @@ class Location extends Controller
 		echo $result ? json_encode($result) : '[{id:"",text:"- เลือกตำบล -"}]';
 	}
 	
-	function ajax_province()
-	{
-		$condition = ($_GET['q'] != 1) ? 'where dpc_id = '.$_GET['q'] : ''; 
-		$result = $this->db->GetArray('select id,province as text from provinces '.$condition.' order by CONVERT(province USING TIS620) asc');
-		echo $result ? json_encode($result) : '[{id:"",text:"- เลือกจังหวัด  -"}]';
-	}
-	
-	function ajax_village()
-	{
-		$result = $this->db->getarray('select id,vill_name as text from villages where district_id = ?',$_GET['q']);
-		echo $result ? json_encode($result) : '[{id:"",text:"- เลือกหมู่บ้าน  -"}]';
-	}
-	
 }
-?>

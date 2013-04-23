156c272a6d4b865d7402f555d1e9efd6bea36965
unknown <Favourite@.(none)>
2013-04-23 21:02:40 +0700

    UPDATE VACANCY IMPORT

--- application/modules/disadvantaged/controllers/disadvantaged.php
+++ application/modules/disadvantaged/controllers/disadvantaged.php
@@ -87,6 +87,146 @@ Class Disadvantaged extends Public_Controller{
 		}
 		
 	}
+		function vacancy_upload()
+		{
+			$ext = pathinfo($_FILES['file_import']['name'], PATHINFO_EXTENSION);
+			
+			$direct_name = 'import_file/vacancy';
+			$file_name = 'vacancy_'.$_POST['YEAR'].'_'.date("Y_m_d_H_i_s").'.'.$ext;
+			
+			#echo $ext;
+			move_uploaded_file($_FILES['file_import']['tmp_name'], $direct_name.'/'.$file_name);
+			$data = $this->ReadData($direct_name.'/'.$file_name);
+			echo $data;
+			print_r($data);
+			unlink($direct_name.'/'.$file_name);
+
+
+			$_FILES['fl_import'] = $_FILES['file_import'];
+/*			
+			if($_FILES['fl_import']['name']!='xxxxx'){
+#			$this->db->execute("DELETE FROM POPULATION_DETAIL WHERE PID IN (SELECT ID FROM POPULATION WHERE PROVINCE_ID=".$_POST['province_id']." AND YEAR_DATA=".$_POST['year_data'].")");
+#			$this->db->execute("DELETE FROM POPULATION WHERE PROVINCE_ID=".$_POST['province_id']." AND YEAR_DATA=".$_POST['year_data']);
+			$ext = pathinfo($_FILES['fl_import']['name'], PATHINFO_EXTENSION);
+			$file_name = 'population_'.$_POST['province_id'].date("Y_m_d_H_i_s").'.'.$ext;
+			$file_name = 'population'.'.'.$ext;			
+			$uploaddir = 'import_file/population/';
+			$fpicname = $uploaddir.$file_name;
+			move_uploaded_file($_FILES['fl_import']['tmp_name'], $fpicname);
+			echo $fpicname;		
+			$data = $this->ReadData($uploaddir.$file_name);			
+			foreach($data as $item):								
+				if($item['value_length']==1712){
+					$chars = str_split($item['value'] , 8);
+					//print_r($chars);	
+					$item['title']."<br>";
+					
+					
+					if (strpos($item['title'], 'จังหวัด')!==false) {
+						$province_name = str_replace('จังหวัด', '', $item['title']);
+						$province = $this->province->where(" province='".iconv('utf-8','tis-620',$province_name)."'")->get_row();
+						$province_id = $province['id'];		
+						$val['PROVINCE_ID'] = $province_id;
+						$val['PROVINCE_NAME'] = $province_name;
+						$val['AMPHUR_ID'] = '';
+						$val['AMPHUR_NAME'] = '';
+						$val['DISTRICT_ID'] = '';
+						$val['DISTRICT_NAME'] = '';			
+						$val['YEAR_DATA'] = $_POST['year_data'];					
+						$val['LUNAR_CAL_MALE'] = (int)$chars[204];
+						$val['LUNAR_CAL_FEMALE'] = (int)$chars[205];
+						$val['CENTRAL_HH_MALE'] = (int)$chars[206];
+						$val['CENTRAL_HH_FEMALE'] = (int)$chars[207];
+						$val['NO_THAI_MALE'] = (int)$chars[208];
+						$val['NO_THAI_FEMALE'] = (int)$chars[209];
+						$val['IN_TRANS_MALE'] = (int)$chars[210];
+						$val['IN_TRANS_FEMALE'] = (int)$chars[211];
+						$val['SUM_MALE'] = (int)$chars[212];
+						$val['SUM_FEMALE'] = (int)$chars[213];
+						$id = $this->ppl->save($val);					
+						
+						for($i=0;$i<=203;$i++){
+							$val_sub['PID'] = $id;
+							$val_sub['AGE_RANGE_CODE'] = $i+1;
+							$val_sub['NUNIT'] = $chars[$i];
+							$this->ppl_detail->save($val_sub);
+						}					
+					}
+					else if (strpos($item['title'], 'อำเภอ')!==false) {
+						$amphur_name = strpos($item['title'], 'อำเภอ')!==false ? str_replace('อำเภอ', '', $item['title']) : strpos($item['title'], 'เทศบาล')!==false;
+						$amphur = $this->amphur->where(" province_id=".$province_id." AND AMPHUR_NAME='".iconv('utf-8','tis-620',$amphur_name)."'")->get_row();
+						$amphur_id = $amphur['id'];
+						$val['PROVINCE_ID'] = $province_id;
+						$val['PROVINCE_NAME'] = $province_name;
+						$val['AMPHUR_ID'] = $amphur_id;
+						$val['AMPHUR_NAME'] = $amphur_name;
+						$val['DISTRICT_ID'] = '';
+						$val['DISTRICT_NAME'] = '';		
+						$val['YEAR_DATA'] = $_POST['year_data'];					
+						$val['LUNAR_CAL_MALE'] = (int)$chars[204];
+						$val['LUNAR_CAL_FEMALE'] = (int)$chars[205];
+						$val['CENTRAL_HH_MALE'] = (int)$chars[206];
+						$val['CENTRAL_HH_FEMALE'] = (int)$chars[207];
+						$val['NO_THAI_MALE'] = (int)$chars[208];
+						$val['NO_THAI_FEMALE'] = (int)$chars[209];
+						$val['IN_TRANS_MALE'] = (int)$chars[210];
+						$val['IN_TRANS_FEMALE'] = (int)$chars[211];
+						$val['SUM_MALE'] = (int)$chars[212];
+						$val['SUM_FEMALE'] = (int)$chars[213];
+						$id = $this->ppl->save($val);					
+						
+						for($i=0;$i<=203;$i++){
+							$val_sub['PID'] = $id;
+							$val_sub['AGE_RANGE_CODE'] = $i+1;
+							$val_sub['NUNIT'] = $chars[$i];
+							$this->ppl_detail->save($val_sub);
+						}				
+					}
+					else if (strpos($item['title'], 'ตำบล')!==false && strpos($item['title'], 'เทศบาล')===false) {
+						$district_name = str_replace('ตำบล', '', $item['title']);
+						$district = $this->district->where(" province_id=".$province_id."  AND DISTRICT_NAME='".iconv('utf-8','tis-620',$district_name)."'")->get_row();
+						$amphur = $this->amphur->get_row($district['amphur_id']);
+						$district_id = $district['id'];
+						$val['PROVINCE_ID'] = $province_id;
+						$val['PROVINCE_NAME'] = $province_name;
+						$val['AMPHUR_ID'] = $amphur['id'];
+						$val['AMPHUR_NAME'] = $amphur['amphur_name'];
+						$val['DISTRICT_ID'] = $district_id;
+						$val['DISTRICT_NAME'] = $district_name;		
+						$val['YEAR_DATA'] = $_POST['year_data'];					
+						$val['LUNAR_CAL_MALE'] = (int)$chars[204];
+						$val['LUNAR_CAL_FEMALE'] = (int)$chars[205];
+						$val['CENTRAL_HH_MALE'] = (int)$chars[206];
+						$val['CENTRAL_HH_FEMALE'] = (int)$chars[207];
+						$val['NO_THAI_MALE'] = (int)$chars[208];
+						$val['NO_THAI_FEMALE'] = (int)$chars[209];
+						$val['IN_TRANS_MALE'] = (int)$chars[210];
+						$val['IN_TRANS_FEMALE'] = (int)$chars[211];
+						$val['SUM_MALE'] = (int)$chars[212];
+						$val['SUM_FEMALE'] = (int)$chars[213];
+						$id = $this->ppl->save($val);					
+						//echo "ID===>".$id."<BR>";
+						
+						for($i=0;$i<=203;$i++){
+							$val_sub['PID'] = $id;
+							$val_sub['AGE_RANGE_CODE'] = $i+1;
+							$val_sub['NUNIT'] = $chars[$i];
+							#$this->ppl_detail->save($val_sub);
+							print_r($val_sub);
+							echo '<HR>';
+						}						
+					}
+									
+				}				
+			endforeach;
+			}*/
+		}
+	
+	
+	function vacancy_import()
+	{
+		$this->template->build('vacancy/vacancy_import');	
+	}
 	//========== VACANCY ==========//
 				
 	function social(){
@@ -112,5 +252,7 @@ Class Disadvantaged extends Public_Controller{
 	function allage_form(){
 		$this->template->build('allage_form');
 	}
+
+
 }
 ?>

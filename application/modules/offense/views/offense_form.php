00cbe33bf05c42e432f273c1356898835622a5ed
unknown <Yotsakon@.(none)>
2013-04-23 21:18:29 +0700

    update preeda

---
+++ application/modules/offense/views/offense_form.php
@@ -0,0 +1,70 @@
+<h2>ข้อมูลกลุ่มเป้าหมาย - เด็กและเยาวชน (เพิ่ม/แก้ไข)</h2>
+<h4>เด็กและเยาวชนที่ถูกดำเนินคดีในสถานพินิจและคุ้มครองเด็กและเยาวชน ตามฐานความผิด <span class="gray">แบบ กรมพินิจ ฐานความผิด</span></h4>
+
+
+
+<?php echo form_open('offense/offense_form'); ?>
+
+
+<table class="tbadd">
+<tr>
+  <th>ปี <span class="Txt_red_12">*</span></th>
+  <td><?php echo form_dropdown('offense_year', get_year_option(2555), $rs['offense_year']); ?></td>
+</tr>
+<tr>
+  <th>เขตจังหวัด &gt; จังหวัด</th>
+  <td>
+  <?php echo form_dropdown('offense_aumphur', (empty($rs['offense_aumphur'])) ? array() : get_option('id', 'amphur_name', 'amphur', 'province_id = '.$rs['offense_aumphur'].' order by amphur_name'), $rs['offense_aumphur'], null, '- เลือกอำเภอ -'); ?>
+    &gt; 
+    <?php echo form_dropdown('offense_province', get_option('id', 'province', 'provinces', '1=1 order by province'), $rs['offense_province'], null, '- เลือกจังหวัด -'); ?> &gt; 
+    </td>
+</tr>
+<tr>
+  <th>ความผิดเกี่ยวกับทรัพย์<span class="Txt_red_12"> *</span></th>
+  <td><?php echo form_input('offense_property', $rs['offense_property']); ?>
+    ราย 
+    </td>
+</tr>
+<tr>
+  <th>ความผิดเกี่ยวกับชีวิตและร่างกาย<span class="Txt_red_12"> *</span></th>
+  <td><?php echo form_input('offense_body', $rs['offense_body']); ?>
+    ราย</td>
+</tr>
+<tr>
+  <th>ความผิดเกี่ยวกับเพศ<span class="Txt_red_12"> *</span></th>
+  <td><?php echo form_input('offense_sex', $rs['offense_sex']); ?>
+    ราย </td>
+</tr>
+<tr>
+  <th>ความผิดเกี่ยวกับความสงบสุข เสรีภาพ ชื่อเสียง และการปกครอง<span class="Txt_red_12"> *</span></th>
+  <td><?php echo form_input('offense_dominance', $rs['offense_dominance']); ?>
+    ราย </td>
+</tr>
+<tr>
+  <th>ความผิดเกี่ยวกับยาเสพติดให้โทษ<span class="Txt_red_12"> *</span></th>
+  <td><?php echo form_input('offense_drug', $rs['offense_drug']); ?>
+    ราย</td>
+</tr>
+<tr>
+  <th>ความผิดเกี่ยวกับอาวุธและวัตถุระเบิด<span class="Txt_red_12"> *</span></th>
+  <td><?php echo form_input('offense_weapon', $rs['offense_weapon']); ?>
+    ราย</td>
+</tr>
+<tr>
+  <th>ความผิดอื่น ๆ<span class="Txt_red_12"> *</span></th>
+  <td><?php echo form_input('offense_etc', $rs['offense_etc']); ?>
+    ราย</td>
+</tr>
+</table>
+
+<div id="btnSave">
+    <?php echo form_hidden('id', $rs['id']); ?>
+    <input type="submit" value="บันทึก" class="btn btn-danger">
+    <input type="button" title="ย้อนกลับ"  value="ย้อนกลับ" class="btn"/>
+</div>
+</form>
+<script>
+    $(function(){
+        $('[name=offense_aumphur]').chainedSelect({parent: '[name=offense_province]',url: 'location/ajax_amphur',value: 'id',label: 'text'});
+    });
+</script>
---
+++ application/modules/poor_age/views/allage_index.php
@@ -0,0 +1,51 @@
+<h2>ข้อมูลกลุ่มเป้าหมาย - ผู้ด้อยโอกาส</h2>
+<h4>คนยากจน (กลุ่มอายุ) <span class="gray">แบบ สคช. คนยากจน (กลุ่มวัย)</span></h4>
+
+<div id="search">
+    <form method="get" action="poor_age/allage">
+
+        <?php echo form_dropdown('year', get_year_option(2555), @$_GET['year'], null, '-- ทุกปี --'); ?>
+
+        <input type="submit" title="ค้นหา" value=" " class="btn_search" />
+  
+    </form>
+</div>
+
+<div id="btnBox">
+    <input type="button" title="เพิ่มรายการ" value=" " onclick="document.location='<?php echo site_url('poor_age/allage_form'); ?>'" class="btn_add">
+</div>
+
+<div id="btnBox"><input type="button" title="นำเข้าข้อมูล"  value=" " onclick="document.location='people.php?act=import'" class="btn_import"/></div>
+
+
+<?php echo $pagination; ?>
+
+<table class="tblist">
+<tr>
+  <th>ลำดับ</th>
+  <th>ปี</th>
+  <th>เขตเมือง</th>
+  <th>เขตชนบท</th>
+  <th>ทั่วประเทศ</th>
+  <th>&nbsp;</th>
+</tr>
+    <?php foreach($result as $key => $item): $key += 1;?>
+<tr>
+  <td><?php echo (empty($_GET['page'])) ? $key : $key + (($_GET['page']-1)*20); ?></td>
+  <td><?php echo $item['year']; ?></td>
+  <td><?php $township = $item['township_child']+$item['township_work']+$item['township_elderly']; echo @number_format($township); ?></td>
+  <td><?php $rural = $item['rural_area_child']+$item['rural_area_work']+$item['rural_area_elderly']; echo @number_format($rural); ?></td>
+  <td><?php $country = $item['country_child']+$item['country_work']+$item['country_elderly']; echo @number_format($country); ?></td>
+  <td>
+  
+            <input type="submit" title="แก้ไขรายการนี้" value=" " class="btn_edit vtip"  onclick="window.location='<?php echo site_url('poor_age/allage_form/'.$item['id']); ?>'" />
+            <input type="submit" title="ลบรายการนี้" value=" " class="btn_delete vtip" onclick="if(confirm('ยืนยันการลบ')){window.location='<?php echo site_url('poor_age/allage_delete/'.$item['id']); ?>';}" />
+    
+    </td>
+</tr>
+
+   <?php endforeach; ?>
+
+</table>
+
+<?php echo $pagination; ?>

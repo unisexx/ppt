4d990827fbebba47d647cbec71e10193c9566f2d
unknown <Yot@.(none)>
2013-04-25 23:20:43 +0700

    dla import

---
+++ application/modules/dla/views/import.php
@@ -0,0 +1,13 @@
+<h2>นำเข้าข้อมูล - อปท.</h2>
+<form action='dla/import' method='POST' enctype="multipart/form-data">
+	<table class="tbadd">
+		<tr>
+		  <th>ไฟล์<span class="Txt_red_12"> *</span></th>
+		  <td><input type='file' name='file'></td>
+		</tr>
+	</table>	
+	<div id="btnSave">
+		<input type="submit" value="บันทึก" class="btn btn-danger">
+		<input type="button" title="ย้อนกลับ"  value="ย้อนกลับ" class="btn"/>
+	</div>
+</form>

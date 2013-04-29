421d12dae5c11cd4ffb2e4bd94931c59323f067d
unisexx <unisexx@hotmail.com>
2013-04-29 09:43:17 +0700

    permission

--- application/modules/setting/controllers/setting.php
+++ application/modules/setting/controllers/setting.php
@@ -9,9 +9,9 @@ Class Setting extends Public_Controller{
 		'set_amphor' => array('label' => 'อำเภอ', 'permission' => array('view','add','edit','delete')),
 		'set_tumbon' => array('label' => 'ตำบล', 'permission' => array('view','add','edit','delete')),
 		'report' => array('label' => 'รายงาน', 'permission' => array('view')),
-		'basic' => array('label' => 'ข้อมูลพื้นฐาน', 'permission' => array('view','add','edit','delete')),
-		'target1' => array('label' => 'ข้อมูลกลุ่มเป้าหมาย 1', 'permission' => array('view','add','edit','delete')),
-		'target2' => array('label' => 'ข้อมูลกลุ่มเป้าหมาย 2', 'permission' => array('view','add','edit','delete')),
+		'basic' => array('label' => 'ข้อมูลพื้นฐาน', 'permission' => array('view','add','edit','delete','import')),
+		'target1' => array('label' => 'ข้อมูลกลุ่มเป้าหมาย 1', 'permission' => array('view','add','edit','delete','import')),
+		'target2' => array('label' => 'ข้อมูลกลุ่มเป้าหมาย 2', 'permission' => array('view','add','edit','delete','import')),
 	);
 	
 	public $crud = array(

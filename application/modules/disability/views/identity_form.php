d2ac46eec728edaf983edc919fe68dd578fa3dd5
unknown <Araya@.(none)>
2013-04-29 10:36:29 +0700

    commit disability

--- application/modules/disability/views/identity_form.php
+++ application/modules/disability/views/identity_form.php
@@ -11,59 +11,61 @@
 <tr>
   <th>กรุงเทพมหานคร <span class="Txt_red_12">*</span></th>
   <td>ชาย
-    <input name="bkk_male" type="text" id="textarea15" value="<?php echo @$rs['bkk_male'] ?>"  style="width:30px;" />
+    <input name="bkk_male" type="text" id="textarea15" value="<?php echo @$rs['bkk_male'] ?>"  style="width:50px;" />
     /
     หญิง
-  <input name="bkk_female" type="text" id="textarea16" value="<?php echo @$rs['bkk_female'] ?>"  style="width:30px;" />
+  <input name="bkk_female" type="text" id="textarea16" value="<?php echo @$rs['bkk_female'] ?>"  style="width:50px;" />
     ราย</td>
 </tr>
 <tr>
   <th>ภาคกลางและภาคตะวันออก <span class="Txt_red_12">*</span></th>
   <td>ชาย
-    <input name="ce_male" type="text" id="textarea" value="<?php echo @$rs['ce_male'] ?>"  style="width:30px;" />
+    <input name="ce_male" type="text" id="textarea" value="<?php echo @$rs['ce_male'] ?>"  style="width:50px;" />
 /
   หญิง
-<input name="ce_female" type="text" id="textarea2" value="<?php echo @$rs['ce_female'] ?>"  style="width:30px;" />
+<input name="ce_female" type="text" id="textarea2" value="<?php echo @$rs['ce_female'] ?>"  style="width:50px;" />
 ราย</td>
 </tr>
 <tr>
   <th>ภาคตะวันออกเฉียงเหนือ <span class="Txt_red_12">*</span></th>
   <td>ชาย
-    <input name="ne_male" type="text" id="textarea5" value="<?php echo @$rs['ne_male'] ?>"  style="width:30px;" />
+    <input name="ne_male" type="text" id="textarea5" value="<?php echo @$rs['ne_male'] ?>"  style="width:50px;" />
 /
   หญิง
-<input name="ne_female" type="text" id="textarea17" value="<?php echo @$rs['ne_female'] ?>"  style="width:30px;" />
+<input name="ne_female" type="text" id="textarea17" value="<?php echo @$rs['ne_female'] ?>"  style="width:50px;" />
 ราย</td>
 </tr>
 <tr>
   <th>ภาคใต้ <span class="Txt_red_12">*</span></th>
   <td>ชาย
-    <input name="s_male" type="text" id="textarea6" value="<?php echo @$rs['s_male'] ?>"  style="width:30px;" />
+    <input name="s_male" type="text" id="textarea6" value="<?php echo @$rs['s_male'] ?>"  style="width:50px;" />
     /
     หญิง
-    <input name="s_female" type="text" id="textarea8" value="<?php echo @$rs['s_female'] ?>"  style="width:30px;" />
+    <input name="s_female" type="text" id="textarea8" value="<?php echo @$rs['s_female'] ?>"  style="width:50px;" />
     ราย</td>
 </tr>
 <tr>
   <th>ภาคเหนือ<span class="Txt_red_12">*</span></th>
   <td>ชาย
-    <input name="n_male" type="text" id="textarea3" value="<?php echo @$rs['n_male'] ?>"  style="width:30px;" />
+    <input name="n_male" type="text" id="textarea3" value="<?php echo @$rs['n_male'] ?>"  style="width:50px;" />
     /
     หญิง
-    <input name="n_female" type="text" id="textarea4" value="<?php echo @$rs['n_female'] ?>"  style="width:30px;" />
+    <input name="n_female" type="text" id="textarea4" value="<?php echo @$rs['n_female'] ?>"  style="width:50px;" />
     ราย</td>
 </tr>
 <tr>
   <th>ไม่ระบุ <span class="Txt_red_12">*</span></th>
   <td>ชาย
-    <input name="i_male" type="text" id="textarea7" value="<?php echo @$rs['i_male'] ?>"  style="width:30px;" />
+    <input name="i_male" type="text" id="textarea7" value="<?php echo @$rs['i_male'] ?>"  style="width:50px;" />
     /
     หญิง
-    <input name="i_female" type="text" id="textarea9" value="<?php echo @$rs['i_female'] ?>"  style="width:30px;" />
+    <input name="i_female" type="text" id="textarea9" value="<?php echo @$rs['i_female'] ?>"  style="width:50px;" />
     ราย</td>
 </tr>
 </table>
-<?php echo (!empty($rs['id']))? form_hidden('update',date('Y-m-d')):form_hidden('create',date('Y-m-d')); ?>
+<?php
+echo form_hidden('id',@$rs['id']);
+ echo (!empty($rs['id']))? form_hidden('update',date('Y-m-d')):form_hidden('create',date('Y-m-d')); ?>
 <div id="btnSave">
 <input type="submit" value="บันทึก" class="btn btn-danger">
 <input type="button" title="ย้อนกลับ"  value="ย้อนกลับ" class="btn"/>

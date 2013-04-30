63ed9f00fbff31150725e37deaf746d3d99db35c
unknown <Teerawat@.(none)>
2013-04-30 11:35:25 +0700

    ssa

--- application/modules/offense/views/population_import_form.php
+++ application/modules/offense/views/population_import_form.php
@@ -1,4 +1,4 @@
-<h2>ข้อมูลกลุ่มเป้าหมาย - เด็กและเยาวชน (นำเข้าข้อมูล) <a href="../../../../import_file/offense/child_offense_list.xls" target="_blank"><img src="media/images/btn_ex_data.png" border="0" style=""></a></h2>
+<h2>ข้อมูลกลุ่มเป้าหมาย - เด็กและเยาวชน (นำเข้าข้อมูล) <a href="import_file/offense/child_offense_list.xls" target="_blank"><img src="media/images/btn_ex_data.png" border="0" style=""></a></h2>
 <h4>ประชากร (คน) </h4>
 <?php echo menu::source($menu_id); ?>
 <form method="post" enctype="multipart/form-data" action="offense/offense_import">

61ed66e3245fa1ae674d783dacdfd0e7e36195b3
Yotsakon <iyottt@gmail.com>
2013-05-31 17:06:32 +0700

    update report dla

--- application/modules/location/controllers/location.php
+++ application/modules/location/controllers/location.php
@@ -23,5 +23,21 @@ class Location extends Controller
         if($type == 'report' and !empty($_GET['q'])) array_unshift($result, array('id' => '', 'text' => $text));
 		echo $result ? json_encode($result) : '[{"id":"","text":"'.$text.'"}]';
 	}
+    
+    function ajax_opt($type=NULL)
+    {
+        $text = '-- ทุกตำบล --';
+        $res = $this->db->GetArray('SELECT OPT_NAME
+        FROM FORM_ALL
+        WHERE PROVINCE_ID = ?
+        AND AMPHUR_ID = ?
+        ORDER BY OPT_NAME', array($_GET['province_id'], $_GET['amphur_id']));
+        dbConvert($res);
+        $result = array();
+        foreach($res as $item) $result[$item['opt_name']] = $item['opt_name'];
+        //array_unshift($result, array('id' => '', 'text' => $text));
+        echo $result ? json_encode($result) : '[{"id":"","text":"'.$text.'"}]';
+
+    }
 	
 }

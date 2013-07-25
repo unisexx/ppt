e567dacd2376d1af618e5c8efced3e1c163da744
iYottt <iyottt@gmail.com>
2013-07-25 16:35:53 +0700

    ...

--- themes/ppt/_menu.php
+++ themes/ppt/_menu.php
@@ -2,6 +2,78 @@
 <ul id="mega-menu-6" class="mega-menu unstyled">
 	<li><a href="dashboard">สถานการณ์ที่สำคัญ</a></li>
 	<li><?php echo anchor('report', 'ข้อมูลรายงานสรุป'); ?></li>
+	
+<li><a href="#">กลุ่มเป้าหมาย</a>
+		<ul>
+			<li><a href="#">เด็กและเยาวชน</a>
+                <ul>
+                    <li><a href="report/welfare">เด็กและเยาวชนในสถาบัน</a></li>
+                    <li><a href="offense/offense_report">เด็กและเยาวชนในสถานพินิจฯ</a></li>
+                    <li><a href="report/dla/child">ปัญหาเด็กและเยาวชนในท้องถิ่น</a></li>
+                </ul>
+            </li>
+            <li><a href="#">สตรี</a>
+                <ul>
+                    <li><a href="report/dla/women">ปัญหาสตรีในท้องถิ่น</a></li>
+                </ul>
+            </li>
+            <li><a href="#">คนพิการ</a>
+                <ul>
+                    <li><a href="report/dla/disabled">ปัญหาคนพิการในท้องถิ่น</a></li>
+                    <li><a href="report/disability/disabled">ปัญหาคนพิการในสถาบัน</a></li>
+                </ul>
+            </li>
+            <li><a href="#">ครอบครัว</a>
+                <ul>
+                    <li><a href="report/dla/family">ปัญหาครอบครัวในท้องถิ่น</a></li>
+                </ul>
+            </li>
+            <li><a href="#">ผู้สูงอายุ</a>
+                <ul>
+                    <li><a href="report/dla/older">ปัญหาผู้สูงอายุในท้องถิ่น</a></li>
+                    <li><a href="report/elder_inmates">ผู้ต้องขังสูงอายุ</a></li>
+                    <li><a href="report/elderly">ผู้สูงอายุในสถาบัน</a></li>
+                </ul>
+            </li>
+            <li><a href="#">ผู้ด้อยโอกาส</a>
+                <ul>
+                    <li><a href="poor_province/poor_report">ผู้มีรายได้ต่ำกว่าเส้นความยากจน</a></li>
+                </ul>
+            </li>
+		</ul>
+</li>
+
+<li><a href="#">เชิงประเด็น</a>
+		<ul>
+			<li><a href="#">ข้อมูลเชิงประเด็น</a>
+                <ul>
+                    <li><a href="report/dla/issue1">ปัญหาด้านที่อยู่อาศัยและสิ่งแวดล้อมในท้องถิ่น</a></li>
+                    <li><a href="report/dla/issue2">ปัญหาด้านสุขภาพอนามัยในท้องถิ่น</a></li>
+                    <li><a href="report/dla/issue3">ปัญหาด้านการศึกษาในท้องถิ่น</a></li>
+                    <li><a href="report/dla/issue4">ปัญหาด้านการมีงานทำและรายได้ในท้องถิ่น</a></li>
+                    <li><a href="report/dla/issue5">ปัญหาด้านความไม่ปลอดภัยในชีวิตและทรัพย์สินในท้องถิ่น</a></li>
+                    <li><a href="report/dla/issue6">ปัญหาด้านวัฒนธรรมและจริยธรรมในท้องถิ่น</a></li>
+                    <li><a href="report/vacancy">จำนวนคนว่างงาน บรรจุงาน และสมัครงาน</a></li>
+                    <li><a href="report/mental">ผู้ป่วยสุขภาพจิต</a></li>
+                    <li><a href="report/family/index?keydata=1">ตัวชี้วัดความจำเป็นขั้นพื้นฐาน(จปฐ)</a></li>
+                </ul>
+            </li>
+		</ul>
+</li>
+
+<li><a href="#">พื้นฐาน</a>
+		<ul>
+			<li><a href="#">ข้อมูลพื้นฐาน</a>
+                <ul>
+                    <li><a href="report/population/summary_rate">แนวโน้มประชากรไทย</a></li>
+                    <li><a href="report/population/age_rate">สถิติประชากรรายอายุ</a></li>
+                    <li><a href="report/population/burden_rate">อัตราส่วนการพึ่งพิงของประชากร</a></li>
+                    <li><a href="report/alien_nation_r">คนต่างด้าวที่ได้รับอนุญาตให้ทำงาน</a></li>
+                </ul>
+            </li>
+		</ul>
+</li>
+	
 	<?php foreach(menu::ls(0, @login_data('user_type_id')) as $cat): ?>
 	<li><a><?php echo $cat['title']; ?></a>
 	    <ul>
